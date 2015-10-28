0b64d9406efb2c4a101ff7274bde165ea9218a0d
Przemysław Kisło <przemyslawlukasz@gmail.com>
2015-10-28 14:09:25 +0100

    configuration for butterknife and dagger

---
+++ app/src/main/java/com/konradkrakowiak/samsungworkshop/UsersListApp.java
@@ -0,0 +1,9 @@
+package com.konradkrakowiak.samsungworkshop;
+
+import android.app.Application;
+
+/**
+ * Created by przemyslawlukasz on 28/10/15.
+ */
+public class UsersListApp extends Application {
+}
---
+++ app/src/main/java/com/konradkrakowiak/samsungworkshop/di/UsersListComponent.java
@@ -0,0 +1,8 @@
+package com.konradkrakowiak.samsungworkshop.di;
+
+/**
+ * Created by przemyslawlukasz on 28/10/15.
+ */
+@Component
+public interface UsersListComponent {
+}
